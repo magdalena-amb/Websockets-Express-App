c0117d4c947a9e615bb4c850b4ff7b8c10b8cd36
Magda Ambrozy <magda.ambrozy@gmail.com>
2019-11-19 19:26:26 +0100

    Add mongo DB

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const socket = require('socket.io');
+const MongoClient = require('mongodb').MongoClient;
 
 
 const server = app.listen( 4000, ()=>{ console.log('Listening to requests on port 4000')});
@@ -8,20 +9,83 @@ const server = app.listen( 4000, ()=>{ console.log('Listening to requests on por
 // Static files
 app.use(express.static('public'));
 
-// Socket setup
-const io = socket(server);
+ // Socket setup
+ const io = socket(server);
+ 
+// Connection URL
+const url = 'mongodb://localhost:27017';
+ 
+// Database Name
+const dbName = 'mongoChat';
 
-io.on('connection', (socket) => {
-    console.log('made socket connection', socket.id)
+// Use connect method to connect to the server
+MongoClient.connect(url,
+    { useUnifiedTopology: true }, 
+    function(err, client) {
+        if(err){ throw err; }
 
-    // handles messages coming from client (data)
-    socket.on('chat', (data)=> {
-        // socket.emit refers to all the sockets connected on the server
-        io.emit('chat', data);
+        console.log("Connected successfully to Mongo DB...");
+    
+        const db = client.db(dbName);
+
+        // Connect to Socket.io
+        io.on('connection', (socket) => {
+            console.log('made socket connection', socket.id)
+
+            // Get the chat DB collection
+            const chat = db.collection('chat');
+
+            // Create function to send status
+            sendStatus = function(s) {
+                socket.emit('status', s);
+            }
+
+            // Get chats from mongo collection
+            chat.find().limit(50).sort({_id:1}).toArray((err, res)=>{
+                if(err){
+                    throw err;
+                }
+                //Emit the messages
+                // io.emit refers to all the sockets connected on the server
+                io.emit('output', res);
+            });
+
+            //Handle input events
+            socket.on('input', (data)=>{
+
+                let name = data.name;
+                let message = data.message;
+
+                // Check for name and message
+                if (name == '' || message == ''){
+                    sendStatus('Please enter a name and message');
+                } else {
+                    // Insert message
+                    chat.insertOne({ 
+                        name:name,
+                        message:message
+                        },
+                        ()=> {
+                        io.emit('output', [data]);
+
+                        // Send status object
+                        sendStatus({
+                            message: 'Message sent',
+                            clear: true
+                        });
+                    });
+                }
+            });
+
+            socket.on('typing', (data)=>{
+            // socket.broadcast.emit refers to this one individual socket that's typing the message
+            socket.broadcast.emit('typing', data);
+            });
+        })  
     });
 
-    socket.on('typing', (data)=>{
-        // socket.broadcast.emit refers to this one individual socket that's typing the message
-        socket.broadcast.emit('typing', data);
-    });
-});
+    
+        
+
+
+
--- public/chat.js
+++ public/chat.js
@@ -1,33 +1,82 @@
-//Make connection
-// this socket is from front-end socket.io that's loaded in index.html
-const socket = io.connect('http://localhost:4000');
-
 //Query DOM
 const message = document.querySelector('#message'),
       name = document.querySelector('#name'),
       btn = document.querySelector('#send'),
       output = document.querySelector('#output'),
-      feedback = document.querySelector('#feedback');
+      feedback = document.querySelector('#feedback'),
+      status = document.querySelector('#status');
 
-// Emit events
-btn.addEventListener('click', ()=> {
+// Set default status
+const statusDefault =  '';
+
+const setStatus = (s) => {
+    // set status
+    status.innerText = s;
+    if ( s !== statusDefault) {
+        const delay = setTimeout(()=> setStatus(statusDefault), 4000);
+    }
+}
+
+//Make connection
+// this socket is from front-end socket.io that's loaded in index.html
+const socket = io.connect('http://localhost:4000');
+
+//Check for connection
+if (socket !== undefined) {
+    console.log('Connected to socket!');
+    socket.on('output', (data) => {
+        //console.log(data);
+        if(data.length) {
+            for(let x = 0; x < data.length; x++) {
+                // Build out message div
+                const message = `<p><strong> ${data[x].name} &nbsp; </strong> ${ data[x].message}</p>`
+                output.insertAdjacentHTML("afterbegin", message);
+                
+            }
+        }
+    });
+
+    socket.on('typing', (data)=> {
+        feedback.innerHTML = `<p> ${data} &nbsp; is typing a message </p>`;
+        });
+
+    //handle input
+    btn.addEventListener('click', ()=> {
     //emits message down the socket to the server
-    socket.emit('chat', {          // name of the message
+    socket.emit('input', {          // name of the message
         message: message.value,    // data
         name: name.value
     });
     message.value = '';
-});
+    });
 
-message.addEventListener('keypress', () => {
-    socket.emit('typing', name.value);
-});
+    // broadcasting typing status
+    message.addEventListener('keypress', (e) => {
+        //e.preventDefault();
+        socket.emit('typing', name.value);
 
-// Listen for events
-socket.on('chat', (data) => {
-    output.innerHTML += `<p><strong> ${data.name} &nbsp; </strong> ${data.message} </p>`
-});
+        if (e.which == 13 || e.keyCode == 13) {
+            socket.emit('input', {          
+                message: message.value,    
+                name: name.value
+            });
+            message.value = '';
+        };
+    }); 
+           
+    
 
-socket.on('typing', (data)=> {
-    feedback.innerHTML = `<p> ${data} &nbsp; is typing a message </p>`;
-})
+
+
+    // Get Status from server
+    socket.on('status', (data)=>{
+        //get message status
+        setStatus((typeof data === 'object') ? data.message : data ); 
+
+        //if status is clear, clear text
+        if(data.clear) {
+            status.value = '';
+        }
+    })
+}
+
